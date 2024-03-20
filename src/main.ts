cd4ca97caeb508e599a7fb000b80b4864708e267
Shubham_Patel <shubhamr1975@gmail.com>
2024-03-20 20:06:37 +0530

    Analysing matrics added

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppRoutingModule } from './app/app.routes'; // Ensure this file exists and has a default export of AppModule
+import { inject } from '@vercel/analytics';
 
 
 
 platformBrowserDynamic().bootstrapModule(AppRoutingModule)
   .catch(err => console.error(err));
+  inject();
